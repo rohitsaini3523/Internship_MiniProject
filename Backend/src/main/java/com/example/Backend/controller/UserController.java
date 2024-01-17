94969bdc970ba4a6579719c462cfd317abc42e2c
rohitsaini3523 <rohitsaini3523@gmail.com>
2024-01-17 16:42:08 +0530

    Added Responses From login and Register

--- Backend/src/main/java/com/example/Backend/controller/UserController.java
+++ Backend/src/main/java/com/example/Backend/controller/UserController.java
@@ -2,10 +2,20 @@ package com.example.Backend.controller;
 
 import com.example.Backend.model.UserLogin;
 import com.example.Backend.model.UserRegister;
+import com.example.Backend.respository.UserRepository;
 import com.example.Backend.services.LoginService;
 import com.example.Backend.services.RegisterService;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.query.Page;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.print.Book;
+import java.awt.print.Pageable;
+
+@Slf4j
 @RestController
 public class UserController {
     RegisterService registerService;
@@ -15,26 +25,30 @@ public class UserController {
         this.registerService = registerService;
         this.loginService = loginService;
     }
+
     @GetMapping("/user/{name}")
     public UserLogin displayUser(@PathVariable("name") String name)
     {
         return this.loginService.getUserDetails(name);
     }
     @PostMapping("/login")
-    public void loginUser(@RequestBody UserLogin userLogin){
+    public ResponseEntity loginUser(@RequestBody UserLogin userLogin){
         boolean userFound = this.loginService.login(userLogin);
         if(userFound)
         {
-            System.out.println("User Login :"+ userLogin.getUsername());
+            log.info("User Login: {}",userLogin.getUsername());
+            return new ResponseEntity<String>("User Logged in Successfully!", HttpStatus.ACCEPTED);
         }
-        else
-        {
-            System.out.println("No User Found!");
-        }
+        log.error("No User Found!");
+        return new ResponseEntity<String>("User Not Found!", HttpStatus.NOT_FOUND);
+
     }
     @PostMapping("/register")
-    public void registerUser(@RequestBody UserRegister userRegister)
+    public ResponseEntity registerUser(@RequestBody UserRegister userRegister)
     {
         this.registerService.register(userRegister);
+        return new ResponseEntity<String>("User Register Successfully!", HttpStatus.CREATED);
+
     }
+
 }
