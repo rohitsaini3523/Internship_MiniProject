0f5190a15b771fe3dacc101f601d6f9a0b1bc95d
rohitsaini3523 <rohitsaini3523@gmail.com>
2024-01-19 13:41:28 +0530

    Updated Cross origin

--- Backend/src/main/java/com/example/Backend/services/RegisterService.java
+++ Backend/src/main/java/com/example/Backend/services/RegisterService.java
@@ -5,6 +5,7 @@ import com.example.Backend.model.UserRegister;
 import com.example.Backend.respository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
 @Slf4j
 @Service
 public class RegisterService {
