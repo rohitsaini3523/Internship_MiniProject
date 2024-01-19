4cc6543d8b422d2c011fd58a35e7549e401096e8
rohitsaini3523 <rohitsaini3523@gmail.com>
2024-01-19 13:40:06 +0530

    Added API information using @operation and @Apiresponses

--- Backend/src/main/java/com/example/Backend/controller/UserController.java
+++ Backend/src/main/java/com/example/Backend/controller/UserController.java
@@ -6,49 +6,53 @@ import com.example.Backend.respository.UserRepository;
 import com.example.Backend.services.LoginService;
 import com.example.Backend.services.RegisterService;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.query.Page;
-import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.print.Book;
-import java.awt.print.Pageable;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @Slf4j
 @RestController
 public class UserController {
+
     RegisterService registerService;
     LoginService loginService;
-    UserController(RegisterService registerService,LoginService loginService)
-    {
+
+    UserController(RegisterService registerService, LoginService loginService) {
         this.registerService = registerService;
         this.loginService = loginService;
     }
 
+    @Operation(summary = "Display user details", description = "Get user details by username")
     @GetMapping("/user/{name}")
-    public UserLogin displayUser(@PathVariable("name") String name)
-    {
+    public UserLogin displayUser(@PathVariable("name") String name) {
         return this.loginService.getUserDetails(name);
     }
+
+    @Operation(summary = "Login user", description = "Authenticate user with provided credentials")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "202", description = "User logged in successfully"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
     @PostMapping("/login")
-    public ResponseEntity loginUser(@RequestBody UserLogin userLogin){
+    public ResponseEntity<String> loginUser(@RequestBody UserLogin userLogin) {
         boolean userFound = this.loginService.login(userLogin);
-        if(userFound)
-        {
-            log.info("User Login: {}",userLogin.getUsername());
-            return new ResponseEntity<String>("User Logged in Successfully!", HttpStatus.ACCEPTED);
+        if (userFound) {
+            log.info("User Login: {}", userLogin.getUsername());
+            return new ResponseEntity<>("User Logged in Successfully!", HttpStatus.ACCEPTED);
         }
         log.error("No User Found!");
-        return new ResponseEntity<String>("User Not Found!", HttpStatus.NOT_FOUND);
-
+        return new ResponseEntity<>("User Not Found!", HttpStatus.NOT_FOUND);
     }
+
+    @Operation(summary = "Register user", description = "Register a new user")
+    @ApiResponse(responseCode = "201", description = "User registered successfully")
     @PostMapping("/register")
-    public ResponseEntity registerUser(@RequestBody UserRegister userRegister)
-    {
+    public ResponseEntity<String> registerUser(@RequestBody UserRegister userRegister) {
         this.registerService.register(userRegister);
-        return new ResponseEntity<String>("User Register Successfully!", HttpStatus.CREATED);
-
+        return new ResponseEntity<>("User Registered Successfully!", HttpStatus.CREATED);
     }
-
 }
