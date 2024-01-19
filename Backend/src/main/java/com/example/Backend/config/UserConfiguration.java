67c58665a93178e8938a5da881cb3dc67540cc6c
rohitsaini3523 <rohitsaini3523@gmail.com>
2024-01-19 13:39:06 +0530

    @Bean :- Api Documentation

--- Backend/src/main/java/com/example/Backend/config/UserConfiguration.java
+++ Backend/src/main/java/com/example/Backend/config/UserConfiguration.java
@@ -1,7 +1,18 @@
 package com.example.Backend.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class UserConfiguration {
+    @Bean
+    @Primary
+    public OpenAPI userOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Login and Registration API")
+                        .description("Endpoint for user login and Registration"));
+    }
 }
