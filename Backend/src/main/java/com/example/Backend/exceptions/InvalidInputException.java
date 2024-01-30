b3f9855faf08e529ef44c681d5085994b856f90e
rohitsaini3523 <rohitsaini3523@gmail.com>
2024-01-30 10:09:12 +0530

    Remove redundant response

--- Backend/src/main/java/com/example/Backend/exceptions/InvalidInputException.java
+++ Backend/src/main/java/com/example/Backend/exceptions/InvalidInputException.java
@@ -3,7 +3,6 @@ package com.example.Backend.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class InvalidInputException extends RuntimeException {
     public InvalidInputException(String message) {
         super(message);
--- Backend/src/main/java/com/example/Backend/exceptions/UserRegistrationException.java
+++ Backend/src/main/java/com/example/Backend/exceptions/UserRegistrationException.java
@@ -3,7 +3,6 @@ package com.example.Backend.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class UserRegistrationException extends RuntimeException {
     public UserRegistrationException(String message) {
         super(message);
